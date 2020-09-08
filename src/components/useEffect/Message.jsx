2597765a0b5c21a3056e90e585e7d3528f4bf8d8
fredycorts7 <fred.cor.14@gmail.com>
2020-09-08 11:54:58 -0500

    precauciones para el useEffect

--- src/components/useEffect/Message.jsx
+++ src/components/useEffect/Message.jsx
@@ -1,16 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Message = () => {
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
+  const { x, y } = coords;
+
   useEffect(() => {
-    console.log('Component Message Mounted');
+    //   console.log('Component Message Mounted');
+    const mouseMove = (e) => {
+      const coords = { x: e.x, y: e.y };
+      //   console.log(coords);
+      setCoords(coords);
+    };
+
+    window.addEventListener('mousemove', mouseMove);
+
     return () => {
-      console.log('Component Message Unmounted');
+      //   console.log('Component Message Unmounted');
+
+      window.removeEventListener('mousemove', mouseMove);
     };
   }, []);
 
   return (
     <div>
       <h2>You're beautiful</h2>
+      <h4>
+        x: {x} y: {y}
+      </h4>
     </div>
   );
 };
