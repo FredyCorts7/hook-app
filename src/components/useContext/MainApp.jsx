b554347f1e5e174c79a1defbda004a6a4759a96b
fredycorts7 <fred.cor.14@gmail.com>
2020-09-10 14:25:06 -0500

    Empezando a usar CreateContext y useContext

--- src/components/useContext/MainApp.jsx
+++ src/components/useContext/MainApp.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import AppRouter from './AppRouter';
+import { UserContext } from './UserContext';
 
 const MainApp = () => {
-  return <AppRouter />;
+  const user = {
+    id: 123,
+    name: 'Fredy C',
+    email: 'fred.cor.14@gmail.com',
+  };
+
+  return (
+    <UserContext.Provider value={user}>
+      <AppRouter />
+    </UserContext.Provider>
+  );
 };
 
 export default MainApp;
